10e3c45b673ffe8fd9cea877ceffea901995089b
Md Raufur Rahim <raufurrahim2000@gmail.com>
2023-06-05 22:07:27 +0600

    Practice 2

---
+++ src/Code Examples/Class 5 practice/Class 5 practice/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Transactions;
+
+/*int x = 5;
+string name = "Md Raufur Rahim";
+double[] ages = new double[5];
+ages[0] = 67.6;
+double myage = ages[0];
+double friends = 500.5;
+float f2 = 3.4f;
+double[] ages2 = new double[] { 0, 5.5, 6.7, 8.8};
+double[] ages3 = { 3.5, 56.6, 543.4, 45.4,friends,f2 };
+Console.WriteLine(ages3[4]);
+for(int i=0;i<ages3.Length;i++)
+{
+    Console.WriteLine(ages3[i]);
+}*/
+
+double x1 = 5.5;
+float x2 = 4.7f;
+double[] x3= new double [] { x1, x2,};
+for (int i = 0; i < x3.Length; i++)
+{
+   // Console.WriteLine(x3[i]);
+}
+
+double[,] p1 = new double[3, 2];
+p1[0, 0] = 5.4;
+p1[0, 1] = 6.2;
+p1[1, 0] = 5.23;
+p1[1, 1] = 7.7;
+p1[2, 0] = 9.9;
+p1[2, 1] = 9.22;
+for(int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        Console.Write(p1[i, j]);
+        Console.Write("   ");
+    }
+    Console.WriteLine();
+        }
---
+++ src/Code Examples/JaggedArray/JaggedArray/Program.cs
@@ -0,0 +1,48 @@
+﻿int[][,] jaggedArray = new int[3][,];
+
+// Creating the first 2x2 matrix
+int[,] matrix1 = new int[,]
+{
+    { 1, 2 },
+    { 3, 4 }
+};
+
+// Creating the second 2x2 matrix
+int[,] matrix2 = new int[,]
+{
+    { 5, 6 },
+    { 7, 8 }
+};
+
+// Creating the third 2x2 matrix
+int[,] matrix3 = new int[,]
+{
+    { 9, 10 },
+    { 11, 12 }
+};
+
+// Assigning the matrices to the jagged array
+jaggedArray[0] = new int[,] { matrix1, matrix2 };
+jaggedArray[1] = new int[,] { matrix2, matrix3 };
+jaggedArray[2] = new int[,] { matrix3, matrix1 };
+
+// Printing the jagged array in a cubic manner
+for (int i = 0; i < jaggedArray.Length; i++)
+{
+    for (int j = 0; j < jaggedArray[i].GetLength(0); j++)
+    {
+        for (int k = 0; k < jaggedArray[i].GetLength(1); k++)
+        {
+            for (int l = 0; l < jaggedArray[i][j, k].GetLength(0); l++)
+            {
+                for (int m = 0; m < jaggedArray[i][j, k].GetLength(1); m++)
+                {
+                    Console.Write(jaggedArray[i][j, k][l, m] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+    Console.WriteLine();
+}
